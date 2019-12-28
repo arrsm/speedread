c1e0a0e785c1f0530450c52a1f7447f023a54b40
scottarrr <oceo908@gmail.com>
2019-12-28 18:50:25 -0500

    chunked into sentences

--- app/src/main/java/com/example/oceo/speedread/SpeedReadUtilities.java
+++ app/src/main/java/com/example/oceo/speedread/SpeedReadUtilities.java
@@ -1,7 +1,20 @@
 package com.example.oceo.speedread;
 
+import android.os.Environment;
+
+import java.io.File;
+
 public class SpeedReadUtilities {
     public static long WPMtoMS(long WPM) {
         return Math.round(1000.0 / (WPM / 60.0));
     }
+
+    public void listFiles() {
+        File sdcard = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File[] files = sdcard.listFiles();
+//        for (int i = 0; i < files.length; i++) {
+//            Log.d("File: ", files[i].getName());
+//        }
+    }
+
 }
