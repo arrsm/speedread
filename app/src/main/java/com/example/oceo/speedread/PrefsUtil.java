414b876fdfe3a9776b3624e83a6e6088af0c5c58
scottarrr <oceo908@gmail.com>
2020-01-01 17:30:24 -0500

    prefs operations updated so that chapter saving happens per book

--- app/src/main/java/com/example/oceo/speedread/PrefsUtil.java
+++ app/src/main/java/com/example/oceo/speedread/PrefsUtil.java
@@ -15,37 +15,53 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class PrefsUtil {
+    private static final String TAG = "PrefsUtil";
 
     /*
-    set up singular methods that take different types.
-    Json for more cmoplex stuff
     think over what/how much we can store here reasonably
+    methods to remove items from prefs
+
      */
 
-    public static void writeChapterToPrefs(Activity activity, int chapter) {
+    public static void writeBookDetailsToPrefs(Activity activity, String bookName, HashMap<String, String> bookDetails) {
+        Log.d(TAG, "I want to write the following book details to prefs key: " + bookName);
+        Log.d(TAG, String.valueOf(bookDetails));
         SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt("chapter", chapter);
-        editor.apply();
-        editor.commit();
+
+        if (!(bookDetails == null)) {
+            Gson gson = new Gson();
+            String bookDetailsJson = gson.toJson(bookDetails);
+            editor.putString(bookName, bookDetailsJson);
+            editor.apply();
+            editor.commit();
+        } else {
+            Log.d(TAG, "null details, not written to prefs");
+        }
+
     }
 
-    public static int readChapterFromPrefs(Activity activity) {
+    public static HashMap<String, String> readBookDetailsFromPrefs(Activity activity, String bookName) {
         SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
-        int chapter = sharedPref.getInt("chapter", 0);
-        Log.d("chapter from prefs", String.valueOf(chapter));
-        return chapter;
+        String bookDetailsJson = sharedPref.getString(bookName, null);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<HashMap<String, String>>() {
+        }.getType();
+        HashMap<String, String> bookDetails = gson.fromJson(bookDetailsJson, type);
+        Log.d("chapter from prefs", String.valueOf(bookDetails));
+        return bookDetails;
     }
 
 
-    public static void writeCurrWordIdxToPrefs(Activity activity, int wordIdx) {
+    public static void writeCurrWordIdxToPrefs(Activity activity, String bookName, int wordIdx) {
         SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putInt("currWordIdx", wordIdx);
         editor.apply();
     }
 
-    public static int readCurrWordIdxFromPrefs(Activity activity) {
+    public static int readCurrWordIdxFromPrefs(Activity activity, String bookName) {
         SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
         int chapter = sharedPref.getInt("currWordIdx", 0);
         return chapter;
@@ -65,29 +81,39 @@ public class PrefsUtil {
 
 
     public static void writeBookToPrefs(Activity activity, String book) {
+        /*
+         * note full paths are kept so a click can just send what path to open
+         */
         Log.d("writing book to prefs", activity.toString());
-        Gson gson = new Gson();
         SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         ArrayList<String> currentBookList = readBooksFromPrefs(activity);
+
         if (currentBookList == null) {
             currentBookList = new ArrayList<String>();
         }
-        currentBookList.add(0, book);
-        String booksListJSON = gson.toJson(currentBookList);
-        editor.putString("books", booksListJSON);
-        editor.apply();
-        editor.commit();
+
+        if (!currentBookList.contains(book)) {
+            Gson gson = new Gson();
+            currentBookList.add(0, book);
+            String booksListJSON = gson.toJson(currentBookList);
+            editor.putString("books", booksListJSON);
+            editor.apply();
+            editor.commit();
+        }
     }
 
     public static ArrayList<String> readBooksFromPrefs(Activity activity) {
-        Gson gson = new Gson();
         SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
         String gsonBooksString = sharedPref.getString("books", null);
 
+        Gson gson = new Gson();
         Type type = new TypeToken<ArrayList<String>>() {
         }.getType();
         ArrayList<String> bookList = gson.fromJson(gsonBooksString, type);
+        if (bookList == null) {
+            bookList = new ArrayList<String>();
+        }
 
         return bookList;
     }
