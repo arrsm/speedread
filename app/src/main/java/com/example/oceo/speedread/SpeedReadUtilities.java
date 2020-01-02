b6ab8b73a1e98d138c06d2b9c912d0d93b25613d
scott <scottmcdede@gmail.com>
2020-01-01 19:10:19 -0500

    housekeeping

--- app/src/main/java/com/example/oceo/speedread/SpeedReadUtilities.java
+++ app/src/main/java/com/example/oceo/speedread/SpeedReadUtilities.java
@@ -1,29 +1,19 @@
 package com.example.oceo.speedread;
 
 import android.os.Environment;
+import android.util.Log;
 
 import java.io.File;
 import java.util.ArrayList;
 
 public class SpeedReadUtilities {
-    public static long WPMtoMS(long WPM) {
-        return Math.round(1000.0 / (WPM / 60.0));
-    }
-
-    public void listFiles() {
-        File sdcard = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        File[] files = sdcard.listFiles();
-//        for (int i = 0; i < files.length; i++) {
-//            Log.d("File: ", files[i].getName());
-//        }
-    }
-
 
     /*
         used to prepend path so that app can find the book in the storaeg
         TODO more robust file openings. sometimes the path is different
-         get this to work for multiple file open apps
-         right now this is what works for my current phone, a OP6
+         get this to work for multiple file open apps right now this is what works for my current phone, a OP6
+         save WPM setting to prefs
+
      */
     static String modifyFilePath(String filePath) {
 
@@ -41,8 +31,6 @@ public class SpeedReadUtilities {
         temp = temp.substring(temp.lastIndexOf('/') + 1);
         temp = temp.replace(".epub", "");
         return temp;
-
-
     }
 
     /*
@@ -56,5 +44,18 @@ public class SpeedReadUtilities {
         }
         return bookNames;
     }
+
+    public static long WPMtoMS(long WPM) {
+        return Math.round(1000.0 / (WPM / 60.0));
+    }
+
+    public void listFiles() {
+        File sdcard = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File[] files = sdcard.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            Log.d("File: ", files[i].getName());
+        }
+    }
+
 
 }
