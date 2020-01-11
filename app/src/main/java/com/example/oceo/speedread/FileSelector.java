b1532be5f907f4ccc5a265950e667ff5877a05b4
scottarrr <oceo908@gmail.com>
2020-01-11 16:56:19 -0500

    push notification start - working

--- app/src/main/java/com/example/oceo/speedread/FileSelector.java
+++ app/src/main/java/com/example/oceo/speedread/FileSelector.java
@@ -2,15 +2,13 @@ package com.example.oceo.speedread;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
 
 
 public class FileSelector extends AppCompatActivity {
