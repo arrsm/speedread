dd3f98b19fbdc70864053beb3524a670e2c112c4
scottarrr <oceo908@gmail.com>
2020-10-27 10:12:22 -0400

    refactor away touch handler to its own class

--- app/src/main/java/com/example/oceo/speedread/TextSelectionUtil.kt
+++ app/src/main/java/com/example/oceo/speedread/TextSelectionUtil.kt
@@ -3,19 +3,23 @@ package com.example.oceo.speedread
 import android.util.Log
 import android.widget.TextView
 
-private fun handleTextSelection(currentChunkView: TextView?,
-                                textSelectionMenu: cTextSelectionMenu?,
-                                chosenFileName: String?,
-                                currentChapter: Int,
-                                currSentenceStart: Int,
-                                mTouchX: Int,
-                                mTouchY: Int) {
-    if (currentChunkView == null) {
-        return
+class TouchSelection() {
+    private val mTouchX = 0
+    private val mTouchY = 0
+    private fun handleTextSelection(currentChunkView: TextView?,
+                                    textSelectionMenu: cTextSelectionMenu?,
+                                    chosenFileName: String?,
+                                    currentChapter: Int,
+                                    currSentenceStart: Int,
+                                    mTouchX: Int,
+                                    mTouchY: Int) {
+        if (currentChunkView == null) {
+            return
+        }
+        if (currentChunkView.hasSelection()) {
+            textSelectionMenu!!.setMetadata(chosenFileName, currentChapter, currSentenceStart)
+            currentChunkView.customSelectionActionModeCallback = textSelectionMenu
+        }
+        Log.d("text selection", "($mTouchX, $mTouchY)")
     }
-    if (currentChunkView!!.hasSelection()) {
-        textSelectionMenu!!.setMetadata(chosenFileName, currentChapter, currSentenceStart)
-        currentChunkView!!.customSelectionActionModeCallback = textSelectionMenu
-    }
-    Log.d("text selection", "($mTouchX, $mTouchY)")
 }
