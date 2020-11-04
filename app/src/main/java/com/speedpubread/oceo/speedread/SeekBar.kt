3f2cf219463e37b4d2170935660764190aa24d12
scottarrr <oceo908@gmail.com>
2020-11-04 13:20:49 -0500

    word offsets saved to prefs

--- app/src/main/java/com/speedpubread/oceo/speedread/SeekBar.kt
+++ app/src/main/java/com/speedpubread/oceo/speedread/SeekBar.kt
@@ -6,19 +6,19 @@ import android.view.View
 import android.widget.SeekBar
 import android.widget.TextView
 
-class Seeker(rootView: View, val reader: Reader) {
+class Seeker(rootView: View, val reader: Reader, val max: Int = 0, val min: Int = 0) {
 
     val chapterSeekBar: SeekBar = rootView.findViewById(R.id.seekBar)
     val chptProgressView: TextView = rootView.findViewById(R.id.chapter_progress_view)
 
     init {
+        chapterSeekBar.max = max
+        chapterSeekBar.min = min
         setupSeekbar()
     }
 
     @SuppressLint("SetTextI18n")
     fun setupSeekbar() {
-        chapterSeekBar.max = reader.maxWordIdx
-        chapterSeekBar.min = 0
         chapterSeekBar.progress = reader.currentWordIdx
         val chptPercentageComplete = getChapterPercentageComplete()
         if (chptPercentageComplete.toString().length > 3) {
@@ -29,36 +29,48 @@ class Seeker(rootView: View, val reader: Reader) {
 
         chapterSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-//                Log.d("seeking: ", String.valueOf(progress));
+                Log.d("seeking: ", "$progress / $max");
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {
 //                Log.d(TAG, "seekBar start tracking touch");
-                reader.disposeListener()
+//                reader.disposeListener()
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar) {
 //                Log.d(TAG, "seekBar stop tracking touch");
-                val progress = seekBar.progress
-                reader.currSentenceStart = reader.getSentenceStartIdx(progress-1)
-                reader.currentWordIdx = reader.currSentenceStart
-                //                reader.currSentenceIdx = getWordPositionInSentence(progress/;
-                reader.currSentenceIdx = 0
+//                val progress = seekBar.progress
+//                reader.currSentenceStart = reader.getSentenceStartIdx(progress - 1)
+//                reader.currentWordIdx = reader.currSentenceStart
+//                //                reader.currSentenceIdx = getWordPositionInSentence(progress/;
+//                reader.currSentenceIdx = 0
+//
+//                val chapterPercentage = getChapterPercentageComplete()
+//                if (chptPercentageComplete.toString().length > 3) {
+//                    chptProgressView.setText(chapterPercentage.toString().substring(0, 4) + "%")
+//                } else {
+//                    Log.d("updating seekbar text", progress.toString())
+//                    chptProgressView.setText("$chapterPercentage%")
+//                }
 
-                val chapterPercentage = getChapterPercentageComplete()
-                if (chptPercentageComplete.toString().length > 3) {
-                    chptProgressView.setText(chapterPercentage.toString().substring(0, 4) + "%")
-                } else {
-                    Log.d("updating seekbar text", progress.toString())
-                    chptProgressView.setText("$chapterPercentage%")
-                }
+
 //                reader.iterateWords(story!!, currentChunkView!!, currentWordView!!, chptProgressView!!, chapterSeekBar!!)
             }
         })
     }
 
+    fun setSeekerMax(maxVal: Int) {
+        chapterSeekBar.max = maxVal
+    }
+
+    fun setSeekerMin(minVal: Int) {
+        chapterSeekBar.min = minVal
+
+    }
+
     fun getChapterPercentageComplete(): Float {
-        return reader.currentWordIdx.toFloat() / reader.maxWordIdx.toFloat() * 100
+        Log.d("the percentage calculation: ", "${reader.currentWordIdx} / $max = ${reader.currentWordIdx.toFloat() / max}")
+        return reader.currentWordIdx.toFloat() / max
     }
 
 }
